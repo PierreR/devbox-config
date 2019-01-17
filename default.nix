389fa9f5f8a5042090affbb3aa265d5ad2a3fc91
Pierre Radermecker <pradermecker@cirb.brussels>
2019-01-17 12:38:50 +0100

    Add default.nix to be used in box.dhall

---
+++ default.nix
@@ -0,0 +1,29 @@
+# You can build this repository by running:
+#   $ nix-build
+with import <nixpkgs> {};
+
+let
+
+# Instead of building from nixpkgs, use the provided static executable directory
+# By relying solely on the static executable, we don't care about the nix machinery that would build from source.
+# Changes are that the version we want is not yet in the binary cache.
+# For instance, building with nix using `nix-env -iA haskellPackages.dhall_1_20_1 -f https://github.com/NixOS/nixpkgs/archive/088cb13aee074fb48d104222cb80fb552ec17529.tar.gz `
+# would currently 250M (unpacked) and then compile dhall ...
+dhall =
+  stdenv.mkDerivation rec {
+    name = "dhall-${version}";
+    version = "1.20.1";
+    src = fetchurl {
+        url = "https://github.com/dhall-lang/dhall-haskell/releases/download/1.20.1/dhall-${version}-x86_64-linux.tar.bz2";
+        sha256 = "1ycb0pglwi9k1pn4qqjyvd4bi54000fb9aqxnn550a515rm6cb03";
+    };
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp dhall $out/bin/
+    '';
+  };
+in
+{
+inherit dhall;
+}
